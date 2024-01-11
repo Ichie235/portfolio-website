0f5e53712d68e1485f97b587f9b38fca3f8c5b9b
Ichie Chinemerem Richard <108372995+Ichie235@users.noreply.github.com>
2024-01-11 15:24:34 +0100

    add react typescript type to package.json. update TextReveal.tsx

--- src/animations/TextReveal.tsx
+++ src/animations/TextReveal.tsx
@@ -1,72 +1,74 @@
-"use client"
+"use client";
 import { useEffect, useRef } from "react";
 import { motion, useInView, useAnimation } from "framer-motion";
 
 type TextRevealProps = {
-    text: string | string[];
-    el?: keyof JSX.IntrinsicElements;
-    width?: "fit-content" | "100%";
-    once?: boolean;
-    className?: string;
-}
+  text: string | string[];
+  el?: keyof JSX.IntrinsicElements;
+  width?: "fit-content" | "100%";
+  once?: boolean;
+  className?: string;
+};
 
 export const TextReveal = ({
-    el: Wrapper = "div",
-    text, width, once, className
+  el: Wrapper = "div",
+  text,
+  width,
+  once,
+  className,
 }: TextRevealProps) => {
-    const ref = useRef<HTMLDivElement | null>(null);
-    const isInView = useInView(ref, { once });
-    const mainControls = useAnimation();
-    const slideControls = useAnimation();
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { once });
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
 
-    useEffect(() => {
-        if(isInView){
-            mainControls.start("visible");
-            slideControls.start("visible");
-        }
-    }, [isInView])
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+  }, [isInView, mainControls, slideControls]);
 
-    return(
-        <Wrapper
-            ref={ref}
-            style={{
-                position: "relative", 
-                width: width, 
-                overflow: "hidden"
-            }}
-            className={className}
+  return (
+    <div ref={ref}>
+      <Wrapper
+        style={{
+          position: "relative",
+          width: width,
+          overflow: "hidden",
+        }}
+        className={className}
+      >
+        <motion.div
+          variants={{
+            hidden: { opacity: 0, y: 75 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate={mainControls}
+          transition={{ duration: 0.5, delay: 0.25 }}
         >
-            <motion.div
-                variants={{
-                    hidden: { opacity: 0, y: 75 },
-                    visible: { opacity: 1, y: 0 },
-                }}
-                initial="hidden"
-                animate={mainControls}
-                transition={{ duration: 0.5, delay: 0.25 }}
-                >
-                {text}
-            </motion.div>
-            <motion.div
-                variants={{
-                    hidden: { left: 0 },
-                    visible: { left: "100%" },
-                }}
-                initial="hidden"
-                animate={slideControls }
-                transition={{ duration: 0.5, ease: "easeIn" }}
-                style={{
-                    position: "absolute",
-                    top: 4,
-                    bottom: 4,
-                    left: 0,
-                    right: 0,
-                    background: "var(--color)",
-                    zIndex: 20,
-                }}
-            >
-
-            </motion.div>
-        </Wrapper>
-    );
+          {text}
+        </motion.div>
+        <motion.div
+          variants={{
+            hidden: { left: 0 },
+            visible: { left: "100%" },
+          }}
+          initial="hidden"
+          animate={slideControls}
+          transition={{ duration: 0.5, ease: "easeIn" }}
+          style={{
+            position: "absolute",
+            top: 4,
+            bottom: 4,
+            left: 0,
+            right: 0,
+            background: "var(--color)",
+            zIndex: 20,
+          }}
+        ></motion.div>
+      </Wrapper>
+    </div>
+  );
 };
