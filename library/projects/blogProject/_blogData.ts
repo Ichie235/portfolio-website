90ab3ddc4c87d6fe3434e247f4b607a941baac88
Ichie Chinemerem Richard <108372995+Ichie235@users.noreply.github.com>
2024-02-10 02:30:58 +0100

    add some data

--- library/projects/blogProject/_blogData.ts
+++ library/projects/blogProject/_blogData.ts
@@ -2,6 +2,8 @@ export type BlogWebsiteProps = {
   id: string;
   paragraph1: string;
   paragraph2: string;
+  CRUD: string;
+  JWT: string;
 };
 
 export const blogData: BlogWebsiteProps[] = [
@@ -10,5 +12,7 @@ export const blogData: BlogWebsiteProps[] = [
     paragraph1:
       "ICHIE-BLOGGONG-API is a dynamic Node.js web application. With its seamless integration of CRUD functionalities, JWT web token authentication, and MongoDB database setup, it offers users a robust   platform to create, share, and explore captivating blog content   ",
     paragraph2: "",
+    CRUD: "ICHIE-BLOGGONG-API empowers users with full control over their content through Create, Read, and Search operations. It allows, users can effortlessly manage their blog entries.",
+    JWT: "Security is paramount, and with JWT web token authentication, users can trust in a secure environment for their blogging endeavors. Each user's identity is verified, ensuring only authorized access to sensitive functionalities.",
   },
 ];
