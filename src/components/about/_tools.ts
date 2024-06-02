23ac3459d45096ef5045186e9576575b15b566c7
Ichie Chinemerem Richard <108372995+Ichie235@users.noreply.github.com>
2024-06-02 23:39:12 +0100

    made image live on the web

--- src/components/about/_tools.ts
+++ src/components/about/_tools.ts
@@ -1,31 +1,31 @@
 export const tools = [
     {
         name: "VS Code",
-        icon: "/tools-icons/visual-studio-code.svg",
+        icon: "https://res.cloudinary.com/dl2hqugpy/image/upload/v1717367775/Tools-Icons/b0ac6manxdzickeqpqhq.svg",
         link: "https://code.visualstudio.com/",
         title: "Code Editor",
     },
     {
         name: "Chrome",
-        icon: "/tools-icons/chrome.svg",
+        icon: "https://res.cloudinary.com/dl2hqugpy/image/upload/v1717367774/Tools-Icons/ixs23knn5z5jta51clie.svg",
         link: "https://www.google.com/chrome/",
         title: "Web Browser",
     },
     {
         name: "Microsoft Edge",
-        icon: "/tools-icons/microsoft-edge.svg",
+        icon: "https://res.cloudinary.com/dl2hqugpy/image/upload/v1717367774/Tools-Icons/qkjl39bf27h91mnay3fk.svg",
         link: "https://www.microsoft.com/en-us/edge?ep=198&form=MA13L7&es=40",
         title: "Web Browser",
     },
     {
         name: "Slack",
-        icon:"/tools-icons/slack.svg",
+        icon:"https://res.cloudinary.com/dl2hqugpy/image/upload/v1717367775/Tools-Icons/xx7xv716ucfq5cvzpnqr.svg",
         link: "https://slack.com/",
         title: "Communication Software",
     },
     {
         name: "Notion",
-        icon: "/tools-icons/notion.svg",
+        icon: "https://res.cloudinary.com/dl2hqugpy/image/upload/v1717367774/Tools-Icons/u8j9uenxgff3qho8nxbd.svg",
         link: "https://www.notion.so/",
         title: "Task Management",
     },
